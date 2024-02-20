87b7f801a91d5ce47717cbadad642a9857f63638
NeLifeASkazka <110575273+NeLifeASkazka@users.noreply.github.com>
2024-02-20 10:54:19 +0200

    lab2_v2

---
+++ lab2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/css.css">
+</head>
+<body>
+  <header id="header" class="header">
+    <div class="container">
+      <p class="story">Story</p>
+      <div class="top-bar">
+        <div class="block">
+          <p class="marcy">Marcy..</p>
+        </div>
+      </div>
+      <div class="nav-menu">
+        <ul class="menu">
+          <li class="link">Home Page</li>
+          <li class="link">About Us</li>
+          <li class="link">Contact Us</li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</body>
+</html>
